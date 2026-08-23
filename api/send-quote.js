4e30b3cf183383659daa5fca1b8f08234df0dd4f
MiPizza Admin <theovendoraville@gmail.com>
2026-08-23 19:31:53 -0400

    Revert sender name format in quote form API

--- api/send-quote.js
+++ api/send-quote.js
@@ -51,7 +51,7 @@ module.exports = async (req, res) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        sender: { name: `${name} (${email})`, email: senderEmail },
+        sender: { name, email: senderEmail },
         to: [{ email: recipientEmail }],
         replyTo: { email, name },
         subject: `New Quote Request-CornersCourier: ${service_type}`,
